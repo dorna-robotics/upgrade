5c8be6db5b750b2a353138c43638f9fd451136e7
smhty <hussein.ty@gmail.com>
2022-04-15 18:20:11 -0700

    dorna_lab

--- dorna_lab/setup.sh
+++ dorna_lab/setup.sh
@@ -4,7 +4,7 @@ rm -rf /home/dorna/Downloads/dorna_lab
 mkdir /home/dorna/Downloads/dorna_lab
 
 # clone the repo
-git clone --branch blockly https://gitlab.com/smhty/dorna_lab.git /home/dorna/Downloads/dorna_lab
+git clone --branch hamed https://gitlab.com/smhty/dorna_lab.git /home/dorna/Downloads/dorna_lab
 
 # create directory
 mkdir /home/dorna/Projects
@@ -13,8 +13,9 @@ mkdir /home/dorna/Projects/script
 mkdir /home/dorna/Projects/path_design
 mkdir /home/dorna/Projects/python
 
-# run the update
-pip3 install -r requirements.txt --upgrade --force-reinstall
-
 # update service
 python3 service.py
+
+# navigate to directory
+cd /home/dorna/Downloads/dorna_lab
+pip3 install -r requirements.txt --upgrade --force-reinstall
