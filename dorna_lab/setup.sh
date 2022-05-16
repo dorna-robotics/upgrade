d13678a78c76bec27e872287b10ec0ba94dada1d
smhty <hussein.ty@gmail.com>
2022-05-16 14:33:57 -0700

    Update setup.sh

--- dorna_lab/setup.sh
+++ dorna_lab/setup.sh
@@ -2,6 +2,7 @@
 # variables
 current_dir=$(pwd)
 dir="/home/dorna/Downloads/dorna_lab"
+dir_temp="/home/dorna/Downloads/dorna_lab_temp"
 repo="https://gitlab.com/smhty/dorna_lab.git"
 branch="hamed"
 project="/home/dorna/Projects"
@@ -12,11 +13,12 @@ cron_path="$dir/dorna_lab/application.py"
 cron_comment="dorna_lab"
 
 # remove and reopen the folder
-rm -rf $dir
-mkdir $dir
+#rm -rf $dir
+#mkdir $dir
 
 # clone the repo
-git clone --branch $branch $repo $dir
+#git clone --branch $branch $repo $dir
+git clone --branch $branch $repo $dir_temp
 
 # create project directory
 mkdir $project
@@ -25,9 +27,16 @@ for val in $project_sub_dir; do
 done
 
 # navigate to directory
-cd $dir
+#cd $dir
+cd $dir_temp
 pip3 install -r requirements.txt --upgrade --force-reinstall
 
 # update service
 cd $current_dir
 python3 service.py -n $cron_name -p $cron_path -c $cron_comment
+
+#Make a backup in "current.old"
+#mv $dir $dir.old
+
+#copy from backup
+#cp -R $dir_temp $dir
