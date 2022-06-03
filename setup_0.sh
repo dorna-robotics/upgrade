583de961374ee4e96a82ca03c85b200be8119e37
smhty <hussein.ty@gmail.com>
2022-06-03 10:18:09 -0700

    Update setup_0.sh

--- setup_0.sh
+++ setup_0.sh
@@ -3,6 +3,9 @@ upgrade="server api dorna_lab"
 
 dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+# make downloads directory
+mkdir /home/dorna/Downloads
+
 # install the requirements
 pip3 install -r $dir/requirements.txt --upgrade
 
@@ -11,12 +14,11 @@ for val in $upgrade; do
     sh setup_0.sh
 done
 
-# add service for setp_1: cron_add(name, comment, path, command)
-#cd $dir
-#python3 -c 'import service; service.cron_add("dorna", "upgrade_setup_1", "'$dir/setup_1.sh'", "sh")' 
-
 # shutdown 
 echo "Turn the controller off and and on to complete the update"
+
+# go to home
+cd ~
 
 # remove the directory
 rm -rf $dir
