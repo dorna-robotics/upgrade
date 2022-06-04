ceb70e8ff6ae79dedecc4fec4ca1857f835f30d8
smhty <hussein.ty@gmail.com>
2022-06-03 22:44:02 -0700

    Update setup_0.sh

--- server/setup_0.sh
+++ server/setup_0.sh
@@ -4,20 +4,15 @@ dir="/home/dorna/app"
 repo="https://github.com/smhty/server.git"
 
 # remove and reopen the folder
-#rm -rf $dir
-#mkdir $dir
+rm -rf $dir
+mkdir $dir
 
 # clone the repo
 git clone $repo $dir
 
-
 # update config.txt and rc.local
 /home/dorna/Downloads/dorna_venv/bin/python3 server.py
 
-# navigate to directory
-cd $dir
-git restore .
-git pull
-
 # run the upgrade
+cd $dir
 sh upgrade.sh
