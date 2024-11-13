784161d878caee5dc5053a94b4e3bf1ec166dd97
smhty <hussein.ty@gmail.com>
2024-11-13 12:07:49 -0800

    kill a_out

--- setup.sh
+++ setup.sh
@@ -15,6 +15,11 @@ current_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # Get the version number from /etc/os-release
 version=$(grep -oP '(?<=VERSION_ID=").*(?=")' /etc/os-release)
 
+####################
+#    kill a.out    # 
+####################
+[ -f ~/app/a.out ] && pkill -9 -f ~/app/a.out
+
 # Compare the version number
 if dpkg --compare-versions "$version" lt 11; then
     echo "##################################################################################################################"
