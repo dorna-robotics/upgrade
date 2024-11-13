7ed64c9f9f330fad6f31b56e69560afb1cb65021
smhty <hussein.ty@gmail.com>
2024-11-13 12:27:57 -0800

    kill

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ version=$(grep -oP '(?<=VERSION_ID=").*(?=")' /etc/os-release)
 #    kill a.out    # 
 ####################
 sudo pkill -9 -f ~/app/a.out
+echo "Killing a.out"
 
 # Compare the version number
 if dpkg --compare-versions "$version" lt 11; then
