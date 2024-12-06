6be661fb208e87eb6781e1703abb887396c7d49d
smhty <hussein.ty@gmail.com>
2024-12-05 17:20:20 -0800

    vision

--- vision/setup.sh
+++ vision/setup.sh
@@ -4,7 +4,7 @@
 ###################
 current_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 dir="/home/dorna/Downloads/vision"
-repo="https://github.com/dorna-robotics/education"
+repo="https://github.com/dorna-robotics/dorna_vision"
 
 ########################
 #    clone the repo    #
