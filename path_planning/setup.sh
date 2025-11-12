3bd25c9ef70aef87f275a9968b4eff833c280be1
smjty25 <65506128+smjty25@users.noreply.github.com>
2025-11-12 23:19:02 +0330

    Update setup.sh

--- path_planning/setup.sh
+++ path_planning/setup.sh
@@ -7,18 +7,17 @@ dir="/home/dorna/Downloads/path_planning"
 repo="https://github.com/dorna-robotics/path_planning"
 
 ########################
-#    clone the repo    #
+#    pull the repo    #
 ########################
-git clone $repo $dir
 # navigate to directory
 cd $dir
 git restore .
 git pull
 
-#clear old build
+# clear old build
 rm -rf build/
 
-#make and install
+# make and install
 cmake --preset rpi-arm64
 cmake --build --preset build-rpi64 -j
 sudo cmake --install build/rpi-arm64
