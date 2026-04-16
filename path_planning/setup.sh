868df2c579e83cbd43b36b464eed0f9dbf24a0d1
smjty25 <65506128+smjty25@users.noreply.github.com>
2026-04-16 14:31:24 +0330

    Update setup.sh

--- path_planning/setup.sh
+++ path_planning/setup.sh
@@ -3,7 +3,14 @@
 #    variables    #
 ###################
 # Load environment variables
-. "$HOME/.bashrc"
+#!/bin/sh
+
+if [ -f "$HOME/.bashrc" ]; then
+    . "$HOME/.bashrc"
+fi
+
+echo "VCPKG_ROOT=$VCPKG_ROOT"
+
 current_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 dir="/home/dorna/Downloads/path_planning"
 repo="https://github.com/dorna-robotics/path_planning"
